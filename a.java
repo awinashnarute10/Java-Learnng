b0c50653309765e8f721721911c19a825634ff09
Awinash Narute <awinashnarute4228@gmail.com>
2022-07-22 16:24:32 +0530

    First commit

---
+++ a.java
@@ -0,0 +1,37 @@
+package Java;
+import java.util.Arrays;
+import java.util.Scanner;
+// pen= 10 book=40
+
+
+public class a {
+ public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("How much money doyou have?");
+    int money = sc.nextInt();
+    if (money < 10)
+     System.out.println("Get out of the store!!");
+     else if ( money < 40)  
+      System.out.println("You can only have pen");
+   
+       else if (money == 40) 
+       System.out.println("You can have 1 book or 4 pens");
+       else if  (money >= 50)
+       System.out.println("You can have both or 5 pens atleast");
+         
+       
+
+     
+      
+
+
+
+   
+       
+    
+   
+    
+    
+    
+ } 
+}
