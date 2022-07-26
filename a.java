8ea1fa60924ed264c18799e2ec428fb8b9ce7f8f
Awinash Narute <awinashnarute4228@gmail.com>
2022-07-26 17:05:13 +0530

    some minor changes

--- a.java
+++ a.java
@@ -1,25 +1,24 @@
 package Java;
 import java.util.Arrays;
 import java.util.Scanner;
-
+// pen= 10 book=40
 
 
 public class a {
  public static void main(String[] args){
-
- int day = 2;
- switch (day) {
-    case 1:
-     System.out.println("Monday");
-        break;
-        case 2:
-        System.out.println("Tuesday");
-        break;
-
- 
-    default:
-        System.out.println("wednesday-sunday");
- }
+    Scanner sc = new Scanner(System.in);
+    System.out.println("How much money doyou have?");
+    int money = sc.nextInt();
+    if (money < 10)
+     System.out.println("Get out of the store!!");
+     else if ( money < 40)  
+      System.out.println("You can only have pen");
+   
+       else if (money == 40) 
+       System.out.println("You can have 1 book or 4 pens");
+       else if  (money >= 50)
+       System.out.println("You can have both or 5 pens atleast");
+         
        
 
      
@@ -27,7 +26,12 @@ public class a {
 
 
 
-  
+   
+       
+    
+   
+    
+    
     
  } 
 }
---
+++ switch1.java
@@ -0,0 +1,26 @@
+package Java;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class switch1 {
+    public static void main(String[] args) {
+        
+    } {
+        
+    
+    int day = 1;
+    switch (day) {
+        case 1:
+        System.out.println("Monday");
+        case 2:
+        System.out.println("Tuesday");
+
+        
+            
+           
+        default:
+        System.out.println("Wednesday-sunday");
+            
+}
+}
+}
