f937bd31205556555c2be3e62d182b547c2268cc
Awinash Narute <awinashnarute4228@gmail.com>
2022-07-31 13:20:24 +0530

    learnt about input buffer

--- randomnumberguesserproject.java
+++ randomnumberguesserproject.java
@@ -3,33 +3,40 @@ import java.util.Scanner;
 
 public class randomnumberguesserproject {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-       
+
         int i = 1;
-        while(i>0){
+        int m = (int) (Math.random() * 100);
+        while (i > 0) {
             System.out.println("guess a number");
-            int n = sc.nextInt();
-            if(n>2579){
-                System.out.println("number greater than correct numever, try again");
-                continue;
-            }
-            if(n<2579){
-                System.out.println("number smaller than correct number, try again");
-                continue;
-            
+            try {
+                Scanner sc = new Scanner(System.in);
+
+                int n = sc.nextInt();
+
+                //
+
+                if (n < 0) {
+                    System.out.println("You chose to quit");
+                    break;
+                } else if (n > m) {
+                    System.out.println("number greater than correct numever, try again");
+
+                } else if (n < m) {
+                    System.out.println("number smaller than correct number, try again");
+                    // continue;
+
+                } else {
+                    System.out.println("correct");
+                    break;
                 }
-                else 
-                System.out.println("correct");
-                break;
+            } catch (Exception e) {
+                System.out.println("Incorrect format please type an integer");
+                continue;
+
             }
-        
-        System.out.println("THE END!!");
         }
 
-
-
-        
+        System.out.println("THE END!!");
     }
-    
 
+}
